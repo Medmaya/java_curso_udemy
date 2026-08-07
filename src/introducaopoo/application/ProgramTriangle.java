4390c11a609a6ad1d2efaeb64250dba8df365284
Medmaya <medmay2019@gmail.com>
2026-08-06 23:37:42 -0300

    Initial commit

---
+++ src/introducaopoo/application/ProgramTriangle.java
@@ -0,0 +1,43 @@
+package introducaopoo.application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import introducaopoo.entities.Triangle;
+
+public class ProgramTriangle {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US); // define o padrao de idioma
+        Scanner sc = new Scanner(System.in); // serve para ler o que o usuario digita
+
+        Triangle x, y; // declarando duas variaveis do tipo Triangle
+        x = new Triangle();
+        y = new Triangle();
+
+        System.out.println("Enter the measures of triangle X: ");
+        x.a = sc.nextDouble();
+        x.b = sc.nextDouble();
+        x.c = sc.nextDouble();
+        System.out.println("Enter the measures of triangle Y: ");
+        y.a = sc.nextDouble();
+        y.b = sc.nextDouble();
+        y.c = sc.nextDouble();
+
+        double areaX = x.area();
+        double areaY = y.area();
+
+        System.out.printf("Triangle X area: %.4f%n", areaX);
+        System.out.printf("Triangle Y area: %.4f%n", areaY);
+
+        if (areaX > areaY) {
+            System.out.println("Larger area: X");
+        }
+        else {
+            System.out.println("Larger area: Y");
+        }
+
+        sc.close();
+    }
+}
