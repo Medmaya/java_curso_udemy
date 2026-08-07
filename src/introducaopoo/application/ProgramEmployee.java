17da1fd418682091dfe51dc430f54048aee8148f
Medmaya <medmay2019@gmail.com>
2026-08-07 00:57:56 -0300

    Exercícios corrigidos

--- src/introducaopoo/application/ProgramEmployee.java
+++ src/introducaopoo/application/ProgramEmployee.java
@@ -9,34 +9,28 @@ import java.util.Scanner;
 public class ProgramEmployee {
 
     public static void main(String[] args) {
-        {
 
-            Locale.setDefault(Locale.US);
-            Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
-            Employee employee = new Employee();
+        Employee employee = new Employee();
 
-            System.out.print("Name: ");
-            employee.name = sc.nextLine();
+        System.out.print("Name: ");
+        employee.name = sc.nextLine();
+        System.out.print("Gross salary: ");
+        employee.grossSalary = sc.nextDouble();
+        System.out.print("Tax: ");
+        employee.tax = sc.nextDouble();
 
-            System.out.print("Gross salary: ");
-            employee.grossSalary = sc.nextDouble();
+        System.out.println();
+        System.out.print("Employee : " + employee);
+        System.out.println();
+        System.out.print("Which percentage to increase salary? ");
+        employee.increaseSalary(sc.nextDouble());
 
-            System.out.print("Tax: ");
-            employee.tax = sc.nextDouble();
+        System.out.println();
+        System.out.println("Updated data: " + employee);
 
-            System.out.println();
-            System.out.print("Employee : " + employee);
-
-            System.out.println();
-            System.out.print("Which percentage to increase salary? ");
-            employee.increaseSalary(sc.nextDouble());
-
-            System.out.println();
-            System.out.println("Updated data: " + employee);
-
-            sc.close();
-        }
-
+        sc.close();
     }
 }
